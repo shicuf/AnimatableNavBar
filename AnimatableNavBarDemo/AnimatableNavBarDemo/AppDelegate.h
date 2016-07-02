0f098da7d5a71c849b1cc060efccaeda6ee4cb32
shicuf <bl.shi@hotmail.com>
2016-07-02 23:15:37 +0800

    Init project

---
+++ AnimatableNavBarDemo/AnimatableNavBarDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AnimatableNavBarDemo
+//
+//  Created by shicuf on 16/7/2.
+//  Copyright © 2016年 Belle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AnimatableNavBarDemo/AnimatableNavBarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnimatableNavBarDemo
+//
+//  Created by shicuf on 16/7/2.
+//  Copyright © 2016年 Belle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
